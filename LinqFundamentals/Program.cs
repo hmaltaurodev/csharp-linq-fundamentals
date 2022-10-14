8564cedca6a6bbea094fa9d1eb183f603bf258d8
Henrique Augusto Maltauro <henrique.maltauro.dev@gmail.com>
2022-10-13 21:47:15 -0300

    Initial Commit

---
+++ LinqFundamentals/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace LinqFundamentals
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine("LINQ Fundamentals");
+
+            Console.ResetColor();
+            Console.WriteLine("\r\nPress any key to end execution . . .");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# LINQ Fundalmentals C#
+
+This is a simple C# Console Application  
+Created with the intention of study LINQ Fundamentals with [Macoratti's lessons](https://www.youtube.com/playlist?list=PLJ4k1IC8GhW0yky43O7TeNwRvaVrHdOmJ)
