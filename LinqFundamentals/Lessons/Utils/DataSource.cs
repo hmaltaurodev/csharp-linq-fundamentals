288f42e6ef3259537de684d100dd1afa4abd8720
Henrique Augusto Maltauro <henrique.maltauro.dev@gmail.com>
2022-10-15 21:38:49 -0300

    feat: Create data source class to define lists that will be used in the examples

---
+++ LinqFundamentals/Lessons/Utils/DataSource.cs
@@ -0,0 +1,40 @@
+﻿namespace LinqFundamentals.Lessons.Utils
+{
+    public class DataSource
+    {
+        public static List<int> GetNumbers()
+        {
+            List<int> numbers = new()
+            {
+                1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384
+            };
+
+            return numbers;
+        }
+
+        public static List<int> GetBlackListNumbers()
+        {
+            List<int> blackList = new()
+            {
+                16, 128, 512, 4096
+            };
+
+            return blackList;
+        }
+
+        public static List<Student> GetStudents()
+        {
+            List<Student> students = new()
+            {
+                new Student() { Name = "Henrique", Age = 28 },
+                new Student() { Name = "Maria", Age = 34 },
+                new Student() { Name = "Amanda", Age = 21 },
+                new Student() { Name = "Manoel", Age = 34 },
+                new Student() { Name = "Carlos", Age = 18 },
+                new Student() { Name = "Alicia", Age = 15 }
+            };
+
+            return students;
+        }
+    }
+}
