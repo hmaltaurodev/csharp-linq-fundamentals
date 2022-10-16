f719b9a44105f3f6ef2ef748a7ba647836d5184e
Henrique Augusto Maltauro <henrique.maltauro.dev@gmail.com>
2022-10-15 21:36:48 -0300

    feat: Create student class for iteration examples

---
+++ LinqFundamentals/Lessons/Utils/Student.cs
@@ -0,0 +1,8 @@
+﻿namespace LinqFundamentals.Lessons.Utils
+{
+    public class Student
+    {
+        public string Name { get; set; } = String.Empty;
+        public int Age { get; set; }
+    }
+}
